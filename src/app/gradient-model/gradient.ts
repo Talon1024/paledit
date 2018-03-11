59550b71ccf1c6a107293fa71a719e29b7de030d
Kevin Caccamo <kevin@ciinet.org>
2018-03-10 22:59:12 -0500

    More work on gradients
    Add addStop method and updateStopIdxs method to Gradient
    Fix stopIndex

--- src/app/gradient-model/gradient.ts
+++ src/app/gradient-model/gradient.ts
@@ -8,19 +8,37 @@ export class GradientStop {
 
 export class Gradient {
   stops:GradientStop[];
+  stopIdxs:number[];
   palRange?:ColourRange;
   reverse:boolean = false;
 
   stopIndex(stop:GradientStop):number {
     let length = this.palRange.getLength();
-    let idx = stop.position * length;
-    if (this.reverse) idx = 1.0 - idx;
+    let stopPos = stop.position;
+    if (this.reverse) stopPos = 1.0 - stopPos;
+    let idx = stopPos * length;
     return idx;
+  }
+
+  addStop(stop:GradientStop) {
+    this.stops.push(stop);
+    this.stops.sort(function (a:GradientStop, b:GradientStop):number {
+      return a.position - b.position;
+    });
+    this.updateStopIdxs();
+  }
+
+  updateStopIdxs() {
+    for (let i = 0; i < this.stops.length; i++) {
+      let stop = this.stops[i];
+      this.stopIdxs[i] = this.stopIndex(stop);
+    }
   }
 
   colourAt(idx:number):Rgbcolour {
     let colour = new Rgbcolour();
     let length = this.palRange.getLength();
+
     return colour;
   }
 }
