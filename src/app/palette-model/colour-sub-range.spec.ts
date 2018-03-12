a1608c569b1dacf22a9a0ed091eaa474ad6bc473
Kevin Caccamo <kevin@ciinet.org>
2018-03-11 20:56:41 -0400

    Add simple constructor and tests for ColourSubRange

--- src/app/palette-model/colour-sub-range.spec.ts
+++ src/app/palette-model/colour-sub-range.spec.ts
@@ -4,4 +4,20 @@ describe('ColourSubRange', () => {
   it('should create an instance', () => {
     expect(new ColourSubRange()).toBeTruthy();
   });
+  it('should get the correct length', () => {
+    let range = new ColourSubRange();
+    range.start = 10;
+    range.end = 16;
+    let expected = 6;
+
+    expect(range.getLength()).toEqual(expected);
+  });
+  it('should get the correct length where start > end', () => {
+    let range = new ColourSubRange();
+    range.start = 16;
+    range.end = 10;
+    let expected = 6;
+
+    expect(range.getLength()).toEqual(expected);
+  });
 });
--- src/app/palette-model/colour-sub-range.ts
+++ src/app/palette-model/colour-sub-range.ts
@@ -2,6 +2,11 @@ export class ColourSubRange {
   start:number;
   end:number;
 
+  constructor(start:number = 0, end:number = 0) {
+    this.start = start & 0xFF;
+    this.end = end & 0xFF;
+  }
+
   getLength():number {
     return Math.abs(this.end - this.start);
   }
