a4cd953123603a6d465840b83b1538c98ad666b0
Kevin Caccamo <kevin@ciinet.org>
2018-04-08 07:13:10 -0400

    Fix MainEditorViewComponent unit tests

--- src/app/main-editor-view/main-editor-view.component.spec.ts
+++ src/app/main-editor-view/main-editor-view.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainEditorViewComponent } from './main-editor-view.component';
+import { PaletteViewComponent } from '../palette-view/palette-view.component';
+import { FormsModule } from '@angular/forms';
+import { GradientEditorComponent } from '../gradient-editor/gradient-editor.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { PaletteIoService } from '../palette-io.service';
+import { SettingsService } from '../settings.service';
+import { KeyboardService } from '../keyboard.service';
 
 describe('MainEditorViewComponent', () => {
   let component: MainEditorViewComponent;
@@ -8,7 +15,9 @@ describe('MainEditorViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainEditorViewComponent ]
+      declarations: [ MainEditorViewComponent, PaletteViewComponent, GradientEditorComponent ],
+      imports: [ FormsModule, HttpClientModule ],
+      providers: [ HttpClient, PaletteIoService, KeyboardService, SettingsService ]
     })
     .compileComponents();
   }));
