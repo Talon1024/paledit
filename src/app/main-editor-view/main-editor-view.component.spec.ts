f184e952234ec251783f90fb48dba4370b2e46f6
Kevin Caccamo <kevin@ciinet.org>
2018-04-19 05:12:55 -0400

    Fix MainEditorViewComponent and spec
    Guard against null collection

--- src/app/main-editor-view/main-editor-view.component.spec.ts
+++ src/app/main-editor-view/main-editor-view.component.spec.ts
@@ -2,12 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainEditorViewComponent } from './main-editor-view.component';
 import { PaletteViewComponent } from '../palette-view/palette-view.component';
+import { PaletteNavigatorComponent } from '../palette-navigator/palette-navigator.component';
 import { FormsModule } from '@angular/forms';
 import { GradientEditorComponent } from '../gradient-editor/gradient-editor.component';
+
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { PaletteIoService } from '../palette-io.service';
 import { SettingsService } from '../settings.service';
 import { KeyboardService } from '../keyboard.service';
+import { PaletteOperationService } from '../palette-operation.service';
 
 describe('MainEditorViewComponent', () => {
   let component: MainEditorViewComponent;
@@ -15,9 +19,20 @@ describe('MainEditorViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainEditorViewComponent, PaletteViewComponent, GradientEditorComponent ],
+      declarations: [
+        MainEditorViewComponent,
+        PaletteViewComponent,
+        GradientEditorComponent,
+        PaletteNavigatorComponent
+      ],
       imports: [ FormsModule, HttpClientModule ],
-      providers: [ HttpClient, PaletteIoService, KeyboardService, SettingsService ]
+      providers: [
+        HttpClient,
+        PaletteIoService,
+        KeyboardService,
+        SettingsService,
+        PaletteOperationService
+      ]
     })
     .compileComponents();
   }));
