2af2a5150fe4b10f53b3795672e5a440e794a1e7
Kevin Caccamo <kevin@ciinet.org>
2018-06-25 08:24:31 -0400

    Initial work on adding more palette operations
    Colourize doesn't always work, and I don't know why.
    De/Saturate hasn't been implemented yet.

--- src/app/palette-operations/palette-operations.component.ts
+++ src/app/palette-operations/palette-operations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaletteOperationService } from '../palette-operation.service';
+import { Rgb, Rgbcolour } from '../palette-model/rgb';
 
 @Component({
   selector: 'app-palette-operations',
@@ -8,11 +9,28 @@ import { PaletteOperationService } from '../palette-operation.service';
 })
 export class PaletteOperationsComponent implements OnInit {
 
-  private curColour: string;
+  private tintColour: string;
+  private colourizeColour: string;
 
   constructor(private palOp: PaletteOperationService) { }
 
   ngOnInit() {
   }
 
+  tint(pct: number) {
+    const actualPct = pct / 100;
+    const colour = Rgbcolour.fromHex(this.tintColour);
+    this.palOp.tint(colour, actualPct);
+  }
+
+  colourize() {
+    const colourizeColour = Rgbcolour.fromHex(this.colourizeColour);
+    this.palOp.colourize(colourizeColour);
+  }
+
+  hueShift(by: number) {
+    this.palOp.shiftHue(by);
+  }
+
+  saturate(by: number) {}
 }
