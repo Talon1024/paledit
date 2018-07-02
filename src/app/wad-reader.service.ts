5dc1293e9bb132cc95367e2b121eef0fc4d26b84
Kevin Caccamo <kevin@ciinet.org>
2018-07-02 02:08:40 -0400

    Fix Doom WAD reader
    Fix bufToStr when buffer contains nulls
    Change readWadFile callback interface to accept an error only
    Export DoomWadLump class
    Make name uppercase in getLump

--- src/app/wad-reader.service.ts
+++ src/app/wad-reader.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-class DoomWadLump {
+export class DoomWadLump {
   name: string;
   offset: number;
   length: number;
@@ -20,34 +20,52 @@ export class WadReaderService {
 
   constructor() { }
 
-  private bufToStr(buf: ArrayBuffer) {
+  private bufToStr(buf: ArrayBuffer): string {
     const view = new Uint8Array(buf);
-    let bufStr = String.fromCharCode.apply(null, view);
-    bufStr = bufStr.trim('\x00', 1);
+
+    let firstNonNull = view.findIndex((n) => n !== 0);
+    if (firstNonNull === -1) { firstNonNull = 0; }
+
+    let lastNonNull = view.findIndex((n, i) => i > firstNonNull && n === 0);
+    if (lastNonNull === -1) { lastNonNull = buf.byteLength; }
+
+    const text = view.slice(firstNonNull, lastNonNull);
+    const bufStr = String.fromCharCode.apply(null, text);
     return bufStr;
   }
 
-  getLump(name: string, number = 1): DoomWadLump {
+  getLump(name: string, number = 1): DoomWadLump | Error {
     const lumpNameCount: {[name: string]: number} = {};
+    const wname = name.toUpperCase();
     for (const lump of this.lumps) {
-      if (lump.name === name) {
-        lumpNameCount[lump.name] = lumpNameCount[lump.name] ? lumpNameCount[lump.name] + 1 : 1;
+      if (lump.name === wname) {
+        lumpNameCount[lump.name] =
+          lumpNameCount[lump.name] != null ?
+          lumpNameCount[lump.name] + 1   : 1;
         if (lumpNameCount[lump.name] === number) {
           return lump;
         }
       }
     }
-    return null;
+    return new TypeError(`No ${name} in this WAD!`);
   }
 
-  readWadFile(file: File, callback: (error: any, result: ArrayBuffer) => void) {
+  readWadFile(file: File, callback: (error: any) => void) {
     const fileReader = new FileReader();
     fileReader.readAsArrayBuffer(file);
     fileReader.onload = () => {
       const wad = fileReader.result as ArrayBuffer;
       // const dView = new DataView(wad);
-      const head = this.readWadHead(wad);
-      this.lumps = this.readWadLumps(wad, head);
+      try {
+        const head = this.readWadHead(wad);
+        this.lumps = this.readWadLumps(wad, head);
+        callback(null);
+      } catch (err) {
+        callback(err);
+      }
+    };
+    fileReader.onerror = () => {
+      callback(fileReader.error);
     };
   }
 
@@ -55,7 +73,7 @@ export class WadReaderService {
     const dView = new DataView(wad);
     const wadType = this.bufToStr(wad.slice(0, 4));
     if (!DoomWadHead.types.includes(wadType)) {
-      return null;
+      throw new TypeError('Not a valid Doom WAD file!');
     }
     const numLumps = dView.getUint32(4, true);
     const dirOffset = dView.getUint32(8, true);
