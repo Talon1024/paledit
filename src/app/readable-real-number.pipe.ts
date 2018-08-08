2e88ec71a8e80295fba313bf2cdcd5e9c751e0f1
Kevin Caccamo <kevin@ciinet.org>
2018-08-08 03:41:44 -0400

    Add digits parameter to ReadableRealNumberPipe
    Also, change ReadableRealNumberPipe template name to 'realnum'

--- src/app/readable-real-number.pipe.ts
+++ src/app/readable-real-number.pipe.ts
@@ -1,17 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'real'
+  name: 'realnum'
 })
 export class ReadableRealNumberPipe implements PipeTransform {
 
-  transform(value: number, args?: any): string {
+  transform(value: number, digits: number = 2): string {
     if (Number.isNaN(value)) {
       return 'NaN';
     }
 
     const pct = value;
-    return pct.toFixed(2);
+    return pct.toFixed(digits);
   }
 
 }
